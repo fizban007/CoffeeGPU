cd9e7f82f423a7d391107e18e5bf1e3c626b1792
fizban007 <fizban007@gmail.com>
2019-06-07 21:14:41 -0400

    Added a configuration parser, and fleshed out comments

---
+++ src/sim_params.cpp
@@ -0,0 +1,45 @@
+#include "cpptoml.h"
+#include "sim_params.h"
+
+namespace Coffee {
+
+sim_params
+parse_config(const std::string& filename) {
+  auto config = cpptoml::parse_file(filename);
+  sim_params defaults, result;
+
+  result.dt = config->get_as<double>("dt").value_or(defaults.dt);
+  result.max_steps = config->get_as<uint64_t>("max_steps")
+                     .value_or(defaults.max_steps);
+  result.data_interval = config->get_as<int>("data_interval")
+                         .value_or(defaults.data_interval);
+
+  auto periodic_boundary =
+      config->get_array_of<bool>("periodic_boundary");
+  if (periodic_boundary) {
+    result.periodic_boundary[0] = (*periodic_boundary)[0];
+    result.periodic_boundary[1] = (*periodic_boundary)[1];
+    result.periodic_boundary[2] = (*periodic_boundary)[2];
+  }
+
+
+    auto guard = config->get_array_of<int64_t>("guard");
+    if (guard)
+      for (int i = 0; i < 3; i++) result.guard[i] = (*guard)[i];
+
+    auto N = config->get_array_of<int64_t>("N");
+    if (N)
+      for (int i = 0; i < 3; i++) result.N[i] = (*N)[i];
+
+    auto lower = config->get_array_of<double>("lower");
+    if (lower)
+      for (int i = 0; i < 3; i++) result.lower[i] = (*lower)[i];
+
+    auto size = config->get_array_of<double>("size");
+    if (size)
+      for (int i = 0; i < 3; i++) result.size[i] = (*size)[i];
+
+  return result;
+}
+
+}  // namespace Coffee
--- src/sim_params.h
+++ src/sim_params.h
@@ -1,23 +1,33 @@
 #ifndef _SIM_PARAMS_H_
 #define _SIM_PARAMS_H_
 
+#include <string>
 #include "data/typedefs.h"
 
 namespace Coffee {
 
+////////////////////////////////////////////////////////////////////////////////
+///  This is the parameter class that contains all of the global simulation
+///  parameters. This structure will be copied to the GPU and will be accessible
+///  to all device kernels.
+////////////////////////////////////////////////////////////////////////////////
 struct sim_params {
   // Simulation parameters
-  Scalar dt = 0.01;
-  int max_steps = 10000;
-  int data_interval = 100;
-  bool periodic_boundary[3] = {false};
+  Scalar dt = 0.01;   ///< Time step size in numerical units
+  uint64_t max_steps = 10000; ///< Total number of timesteps
+  int data_interval = 100; ///< How many steps between data outputs
+  bool periodic_boundary[3] = {false}; ///< Periodic boundary condition
 
   // Grid parameters
-  int N[3] = {1};
-  int guard[3] = {0};
-  Scalar lower[3] = {0.0};
-  Scalar size[3] = {1.0};
+  int N[3] = {1};  ///< Number of cells in each direction (excluding guard cells)
+  int guard[3] = {0}; ///< Number of guard cells at each boundary
+  Scalar lower[3] = {0.0}; ///< Lower limits of the simulation box
+  Scalar size[3] = {1.0}; ///< Sizes of the simulation box
 };
+
+
+/// Reads a toml config file, parses the above data structure, and returns it.
+sim_params parse_config(const std::string& filename);
 
 }
 
