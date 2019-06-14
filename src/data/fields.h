73299aee5d1b2f83f2573f58888e7f9537b931b2
fizban007 <fizban007@gmail.com>
2019-06-14 17:53:03 -0400

    Added lots of comments for the fields header

--- src/data/fields.h
+++ src/data/fields.h
@@ -20,21 +20,40 @@ class vector_field {
   vector_field(const self_type &field);
   vector_field(self_type &&field);
 
+  // Copy assignment operator
   self_type &operator=(const self_type &field);
+
+  // Move assignment operator
   self_type &operator=(self_type &&field);
 
-  /// Core functions
+  /// Initialize all values to zero
   void initialize();
+
+  /// Initialize a given component according to function f. f should accept 3
+  /// parameters x, y, z, and return a single scalar.
   template <typename Func>
   void initialize(int component, const Func &f);
+
+  /// Initialize all components according to function f. f should accept 4
+  /// parameters, n, x, y, z, where n is the component. f should again return a
+  /// single value.
   template <typename Func>
   void initialize(const Func &f);
 
+  /// Assign a single value to component n at all points
   void assign(T value, int n);
+
+  /// Assign a single value to all components at all points
   void assign(T value);
+
+  /// Assign the value of another field to this one, multiplied by factor q
   void assign(const vector_field<T> &field, const T &q);
+
+  /// Copy from the other field
   void copy_from(const self_type &field);
 
+  /// Resize this field according to the given grid (and point the grid pointer
+  /// to this new grid)
   void resize(const Grid &grid);
 
   /// Arithmetic operations
@@ -53,17 +72,22 @@ class vector_field {
   // according to m_stagger, and store the result to @result
   void interpolate_from_center(self_type &result, Scalar q = 1.0);
 
-  /// Index operator
-  T &operator()(int n, int x, int y = 0, int z = 0) {
-    return m_data[n](x, y, z);
+  /// Index using three integers, and a component
+  T &operator()(int n, int i, int j = 0, int k = 0) {
+    return m_data[n](i, j, k);
   }
-  const T &operator()(int n, int x, int y = 0,
-                              int z = 0) const {
-    return m_data[n](x, y, z);
+  /// Index using three integers, and a component, const version
+  const T &operator()(int n, int i, int j = 0,
+                              int k = 0) const {
+    return m_data[n](i, j, k);
   }
+
+  /// Index using an Index object, and a component
   T &operator()(int n, const Index &idx) {
     return m_data[n](idx);
   }
+
+  /// Index using an Index object, and a component, const version
   const T &operator()(int n, const Index &idx) const {
     return m_data[n](idx);
   }
@@ -77,7 +101,8 @@ class vector_field {
 
   void set_stagger(int n, Stagger stagger) { m_stagger[n] = stagger; }
   void set_stagger(Stagger stagger[]) {
-    m_stagger = stagger;
+    for (int i = 0; i < 3; i++)
+      m_stagger[i] = stagger[i];
   }
 
  private:
