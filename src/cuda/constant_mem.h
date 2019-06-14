ca80f0975afacbb77f0bd75addd25a6c398f4bec
fizban007 <fizban007@gmail.com>
2019-06-14 14:27:49 -0400

    Added some comments

--- src/cuda/constant_mem.h
+++ src/cuda/constant_mem.h
@@ -8,7 +8,10 @@ namespace Coffee {
 // This is the simulation parameters in constant memory
 extern __device__ __constant__ sim_params dev_params;
 
+// Copy a given parameter struct to constant memory. This can be used to update
+// the parameters on device even after initializing the simulation
+void init_dev_params(const sim_params& params);
 
-}
+}  // namespace Coffee
 
 #endif  // _CONSTANT_MEM_H_
