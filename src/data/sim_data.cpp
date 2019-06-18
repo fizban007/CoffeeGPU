8556c52e04c965997e03e361065cb6e5ca4ca6b1
fizban007 <fizban007@gmail.com>
2019-06-18 15:55:27 -0400

    Defined B0 and added stagger definitions too

--- src/data/sim_data.cpp
+++ src/data/sim_data.cpp
@@ -4,8 +4,12 @@
 namespace Coffee {
 
 sim_data::sim_data(const sim_environment& e) : env(e) {
-  E = vector_field<Scalar>(e.grid());
-  B = vector_field<Scalar>(e.grid());
+  E = vector_field<Scalar>(env.grid());
+  B = vector_field<Scalar>(env.grid());
+  B0 = vector_field<Scalar>(env.grid());
+  Stagger st_b[3] = {Stagger(0b001), Stagger(0b010), Stagger(0b100)};
+  B.set_stagger(st_b);
+  B0.set_stagger(st_b);
 }
 
 sim_data::~sim_data() {}
