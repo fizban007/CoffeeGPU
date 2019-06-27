8dc8ab972d030f0c40bbba6ee6f925aadec67dba
haykh <hayk.hakopyan@gmail.com>
2019-06-27 12:47:17 -0400

    b0 = 0

--- src/user_emwave.hpp
+++ src/user_emwave.hpp
@@ -30,21 +30,21 @@ data.E.initialize(2, [&](Scalar x, Scalar y, Scalar z) {
   return 0.0;
 });
 
-data.B0.initialize(0, [&](Scalar x, Scalar y, Scalar z) {
+data.B.initialize(0, [&](Scalar x, Scalar y, Scalar z) {
   // Put your initial condition for Bx here
   return 0.0;
 });
 
-data.B0.initialize(1, [&](Scalar x, Scalar y, Scalar z) {
+data.B.initialize(1, [&](Scalar x, Scalar y, Scalar z) {
   // Put your initial condition for By here
   return 0.0;
 });
 
-data.B0.initialize(2, [&](Scalar x, Scalar y, Scalar z) {
+data.B.initialize(2, [&](Scalar x, Scalar y, Scalar z) {
   // Put your initial condition for Bz here
   return std::sin(kx * x + ky * y);
 });
 
-data.B.copy_from(data.B0);
+// data.B.copy_from(data.B0);
 
 #endif  // _USER_INIT_H_
