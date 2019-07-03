664bb67ee3447acdc44b9e288f49a7198a582dea
haykh <hayk.hakopyan@gmail.com>
2019-07-03 00:41:58 -0400

    sync

--- src/user_loop.hpp
+++ src/user_loop.hpp
@@ -1,21 +1,6 @@
 #ifndef _USER_INIT_H_
 #define _USER_INIT_H_
 
-// Scalar getMonopole(Scalar x, Scalar y, Scalar z,
-//                    Scalar xc, Scalar yc, Scalar zc,
-//                    int pol, int component) {
-//
-//   if (component == 0) {
-//     return pol * rx / (r * r * r);
-//   } else if (component == 1) {
-//     return pol * ry / (r * r * r);
-//   } else if (component == 2) {
-//     return pol * rz / (r * r * r);
-//   } else {
-//     return -1.0;
-//   }
-// }
-
 Scalar xc = env.params().size[0] * 0.5;
 Scalar yc1 = env.params().size[1] * 0.5 - env.params().a0 * env.params().L0;
 Scalar yc2 = env.params().size[1] * 0.5 + env.params().a0 * env.params().L0;
@@ -36,7 +21,7 @@ data.E.initialize(2, [](Scalar x, Scalar y, Scalar z) {
   return 0.0;
 });
 
-data.B0.initialize(0, [](Scalar x, Scalar y, Scalar z) {
+data.B0.initialize(0, [&](Scalar x, Scalar y, Scalar z) {
   // Put your initial condition for Bx here
   Scalar rx = (x - xc);
   Scalar ry1 = (y - yc1);
@@ -48,7 +33,7 @@ data.B0.initialize(0, [](Scalar x, Scalar y, Scalar z) {
   return rx / (r1 * r1 * r1) - rx / (r2 * r2 * r2);
 });
 
-data.B0.initialize(1, [](Scalar x, Scalar y, Scalar z) {
+data.B0.initialize(1, [&](Scalar x, Scalar y, Scalar z) {
   // Put your initial condition for By here
   Scalar rx = (x - xc);
   Scalar ry1 = (y - yc1);
@@ -60,7 +45,7 @@ data.B0.initialize(1, [](Scalar x, Scalar y, Scalar z) {
   return ry1 / (r1 * r1 * r1) - ry2 / (r2 * r2 * r2);
 });
 
-data.B0.initialize(2, [](Scalar x, Scalar y, Scalar z) {
+data.B0.initialize(2, [&](Scalar x, Scalar y, Scalar z) {
   // Put your initial condition for Bz here
   Scalar rx = (x - xc);
   Scalar ry1 = (y - yc1);
