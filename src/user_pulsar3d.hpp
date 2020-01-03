88115aae97a8cf5c40d9ae7cf2883f9d419bfcf7
Yajie Yuan <yuanyajie.yyj@gmail.com>
2020-01-03 14:26:49 -0500

    debug

--- src/user_pulsar3d.hpp
+++ src/user_pulsar3d.hpp
@@ -7,16 +7,18 @@
 for (int i = 0; i < 3; ++i) {
   data.B.initialize(i, [&](Scalar x, Scalar y, Scalar z) {
     // Put your initial condition for Bx here
-    // return env.params().b0 * cube(env.params().radius) *
-    //        dipole_x(x, y, z, env.params().alpha, 0);
+
+    // return env.params().b0 *
+    //        (dipole2(x, y, z, env.params().p1, env.params().p2,
+    //                 env.params().p3, 0, i) +
+    //         quadrupole(x, y, z, env.params().q11, env.params().q12,
+    //                    env.params().q13, env.params().q22,
+    //                    env.params().q23, env.params().q_offset_x,
+    //                    env.params().q_offset_y, env.params().q_offset_z,
+    //                    0, i));
     return env.params().b0 *
-           (dipole2(x, y, z, env.params().p1, env.params().p2,
-                    env.params().p3, 0, i) +
-            quadrupole(x, y, z, env.params().q11, env.params().q12,
-                       env.params().q13, env.params().q22,
-                       env.params().q23, env.params().q_offset_x,
-                       env.params().q_offset_y, env.params().q_offset_z,
-                       0, i));
+           dipole2(x, y, z, env.params().p1, env.params().p2,
+                    env.params().p3, 0, i);
   });
 }
 
