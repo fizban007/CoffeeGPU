3b30058879ac06f6943cc3259bf95f4bc7745254
Yajie Yuan <yuanyajie.yyj@gmail.com>
2020-01-14 14:46:56 -0500

    changed tiny in metric

--- src/algorithms/field_solver_gr_EZ.cu
+++ src/algorithms/field_solver_gr_EZ.cu
@@ -33,8 +33,8 @@ get_r(Scalar a, Scalar x, Scalar y, Scalar z) {
   Scalar R2 = get_R2(x, y, z);
   return std::sqrt(
       (R2 - a * a +
-       std::sqrt(std::max(square(R2 - a * a) + 4.0 * square(a * z), TINY))) /
-      2.0);
+       std::sqrt(std::max(square(R2 - a * a) + 4 * square(a * z), TINY))) /
+      2);
 }
 
 HOST_DEVICE Scalar
@@ -45,48 +45,48 @@ get_g() {
 HOST_DEVICE Scalar
 get_beta_d1(Scalar a, Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(a, x, y, z);
-  return 2.0 * r * r * r * (r * x + a * y) / std::max(a * a + r * r, TINY) /
+  return 2 * r * r * r * (r * x + a * y) / std::max(a * a + r * r, TINY) /
          std::max(r * r * r * r + a * a * z * z, TINY);
 }
 
 HOST_DEVICE Scalar
 get_beta_d2(Scalar a, Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(a, x, y, z);
-  return 2.0 * r * r * r * (-a * x + r * y) / std::max(a * a + r * r, TINY) /
+  return 2 * r * r * r * (-a * x + r * y) / std::max(a * a + r * r, TINY) /
          std::max(r * r * r * r + a * a * z * z, TINY);
 }
 
 HOST_DEVICE Scalar
 get_beta_d3(Scalar a, Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(a, x, y, z);
-  return 2.0 * r * r * z / std::max(r * r * r * r + a * a * z * z, TINY);
+  return 2 * r * r * z / std::max(r * r * r * r + a * a * z * z, TINY);
 }
 
 HOST_DEVICE Scalar
 get_beta_u1(Scalar a, Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(a, x, y, z);
-  return 2.0 * r * r * r * (r * x + a * y) / std::max(a * a + r * r, TINY) /
-         std::max(2.0 * r * r * r + r * r * r * r + a * a * z * z, TINY);
+  return 2 * r * r * r * (r * x + a * y) / std::max(a * a + r * r, TINY) /
+         std::max(2 * r * r * r + r * r * r * r + a * a * z * z, TINY);
 }
 
 HOST_DEVICE Scalar
 get_beta_u2(Scalar a, Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(a, x, y, z);
-  return 2.0 * r * r * r * (-a * x + r * y) / std::max(a * a + r * r, TINY) /
-         std::max(2.0 * r * r * r + r * r * r * r + a * a * z * z, TINY);
+  return 2 * r * r * r * (-a * x + r * y) / std::max(a * a + r * r, TINY) /
+         std::max(2 * r * r * r + r * r * r * r + a * a * z * z, TINY);
 }
 
 HOST_DEVICE Scalar
 get_beta_u3(Scalar a, Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(a, x, y, z);
-  return 2.0 * r * r * z /
-         std::max(2.0 * r * r * r + r * r * r * r + a * a * z * z, TINY);
+  return 2 * r * r * z /
+         std::max(2 * r * r * r + r * r * r * r + a * a * z * z, TINY);
 }
 
 HOST_DEVICE Scalar
 get_gamma(Scalar a, Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(a, x, y, z);
-  return 1.0 + 2.0 * r * r * r / std::max(r * r * r * r + a * a * z * z, TINY);
+  return 1 + 2 * r * r * r / std::max(r * r * r * r + a * a * z * z, TINY);
 }
 
 HOST_DEVICE Scalar
@@ -152,7 +152,7 @@ get_gamma_u11(Scalar a, Scalar x, Scalar y, Scalar z) {
   return 1.0 -
          2.0 * r * r * r * square(r * x + a * y) /
              std::max(square(a * a + r * r), TINY) /
-             std::max(2.0 * r * r * r + r * r * r * r + a * a * z * z, TINY);
+             std::max(2 * r * r * r + r * r * r * r + a * a * z * z, TINY);
 }
 
 HOST_DEVICE Scalar
@@ -160,14 +160,14 @@ get_gamma_u12(Scalar a, Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(a, x, y, z);
   return -2.0 * r * r * r * (r * x + a * y) * (-a * x + r * y) /
          std::max(square(a * a + r * r), TINY) /
-         std::max(2.0 * r * r * r + r * r * r * r + a * a * z * z, TINY);
+         std::max(2 * r * r * r + r * r * r * r + a * a * z * z, TINY);
 }
 
 HOST_DEVICE Scalar
 get_gamma_u13(Scalar a, Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(a, x, y, z);
   return -2.0 * r * r * (r * x + a * y) * z / std::max(a * a + r * r, TINY) /
-         std::max(2.0 * r * r * r + r * r * r * r + a * a * z * z, TINY);
+         std::max(2 * r * r * r + r * r * r * r + a * a * z * z, TINY);
 }
 
 HOST_DEVICE Scalar
@@ -176,14 +176,14 @@ get_gamma_u22(Scalar a, Scalar x, Scalar y, Scalar z) {
   return 1.0 -
          2.0 * r * r * r * square(a * x - r * y) /
              std::max(square(a * a + r * r), TINY) /
-             std::max(2.0 * r * r * r + r * r * r * r + a * a * z * z, TINY);
+             std::max(2 * r * r * r + r * r * r * r + a * a * z * z, TINY);
 }
 
 HOST_DEVICE Scalar
 get_gamma_u23(Scalar a, Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(a, x, y, z);
   return 2.0 * r * r * (a * x - r * y) * z / std::max(a * a + r * r, TINY) /
-         std::max(2.0 * r * r * r + r * r * r * r + a * a * z * z, TINY);
+         std::max(2 * r * r * r + r * r * r * r + a * a * z * z, TINY);
 }
 
 HOST_DEVICE Scalar
@@ -191,7 +191,7 @@ get_gamma_u33(Scalar a, Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(a, x, y, z);
   return 1.0 -
          2.0 * r * z * z /
-             std::max(2.0 * r * r * r + r * r * r * r + a * a * z * z, TINY);
+             std::max(2 * r * r * r + r * r * r * r + a * a * z * z, TINY);
 }
 
 __device__ inline Scalar
