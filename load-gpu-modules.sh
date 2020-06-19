4cb3e15a7b97de8220ce1514ab008c255265618c
Yajie Yuan <yuanyajie.yyj@gmail.com>
2020-06-19 12:11:36 -0400

    trying new modules on rusty

---
+++ load-gpu-modules.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# module load gcc
+# module load openmpi/1.10.7-hfi
+# module load lib/hdf5/1.8.21-openmpi1
+# module load lib/boost/1.68-gcc7
+
+module load gcc/8.4.0
+module load openmpi2/2.1.6-hfi
+module load lib/hdf5/1.12.0-openmpi2
+
+module load cuda/10.1.243_418.87.00
+module load cmake/3.14.5
+
