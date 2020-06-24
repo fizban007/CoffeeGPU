e2cf9ead10198a45f53bc487b4880c8011a20cfc
Yajie Yuan <yuanyajie.yyj@gmail.com>
2020-06-23 21:04:10 -0400

    changed slice output

--- src/utils/data_exporter.cpp
+++ src/utils/data_exporter.cpp
@@ -418,7 +418,7 @@ void data_exporter::add_slice_x(multi_array<Scalar>& array,
     std::cout << name << " slice value at (100,100) " << tmp_slice_x(100, 100)
               << std::endl;
   }
-
+  MPI_Barrier(comm);
   file.write(tmp_slice_x, name);
 }
 
