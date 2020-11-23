90effd416e5c3017471e126aa09ffa4b650f252c
Yajie Yuan <yuanyajie.yyj@gmail.com>
2020-11-22 20:07:58 -0500

    In Wald setup, again set D^i=0

--- src/user_wald2.hpp
+++ src/user_wald2.hpp
@@ -18,28 +18,28 @@ data.B.initialize(2, [&](Scalar x, Scalar y, Scalar z) {
   return 1.0 / get_sqrt_gamma(a0, x, y, z);
 });
 
-// data.E.initialize();
+data.E.initialize();
 
-data.E.initialize(0, [&](Scalar x, Scalar y, Scalar z) {
-  Scalar Bdz = get_gamma_d33(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z);
-  Scalar Bdy = get_gamma_d23(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z);
-  return - 1.0 / get_alpha(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z) *
-         (get_beta_d2(a0, x, y, z) * Bdz - get_beta_d3(a0, x, y, z) * Bdy);
-});
+// data.E.initialize(0, [&](Scalar x, Scalar y, Scalar z) {
+//   Scalar Bdz = get_gamma_d33(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z);
+//   Scalar Bdy = get_gamma_d23(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z);
+//   return - 1.0 / get_alpha(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z) *
+//          (get_beta_d2(a0, x, y, z) * Bdz - get_beta_d3(a0, x, y, z) * Bdy);
+// });
 
-data.E.initialize(1, [&](Scalar x, Scalar y, Scalar z) {
-  Scalar Bdz = get_gamma_d33(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z);
-  Scalar Bdx = get_gamma_d13(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z);
-  return - 1.0 / get_alpha(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z) *
-         (get_beta_d3(a0, x, y, z) * Bdx - get_beta_d1(a0, x, y, z) * Bdz);
-});
+// data.E.initialize(1, [&](Scalar x, Scalar y, Scalar z) {
+//   Scalar Bdz = get_gamma_d33(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z);
+//   Scalar Bdx = get_gamma_d13(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z);
+//   return - 1.0 / get_alpha(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z) *
+//          (get_beta_d3(a0, x, y, z) * Bdx - get_beta_d1(a0, x, y, z) * Bdz);
+// });
 
-data.E.initialize(2, [&](Scalar x, Scalar y, Scalar z) {
-  Scalar Bdx = get_gamma_d13(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z);
-  Scalar Bdy = get_gamma_d23(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z);
-  return - 1.0 / get_alpha(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z) *
-         (get_beta_d1(a0, x, y, z) * Bdy - get_beta_d2(a0, x, y, z) * Bdx);
-});
+// data.E.initialize(2, [&](Scalar x, Scalar y, Scalar z) {
+//   Scalar Bdx = get_gamma_d13(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z);
+//   Scalar Bdy = get_gamma_d23(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z);
+//   return - 1.0 / get_alpha(a0, x, y, z) / get_sqrt_gamma(a0, x, y, z) *
+//          (get_beta_d1(a0, x, y, z) * Bdy - get_beta_d2(a0, x, y, z) * Bdx);
+// });
 
 data.E.sync_to_device();
 
