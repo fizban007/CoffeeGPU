6f30e7592b38e60a5752620e8b8e0f8e35e4481f
fizban007 <fizban007@gmail.com>
2021-03-30 11:20:00 -0400

    small fix of c++ standard version

--- src/algorithms/metric_sph.h
+++ src/algorithms/metric_sph.h
@@ -14,12 +14,16 @@
 namespace Coffee {
 
 #ifdef USE_SIMD
-inline simd::Vec_f_t pos_simd(const Grid& g, int i, int n, int stagger) {
-  simd::Vec_f_t result = simd::vec_inc * g.delta[i] + g.pos(i, n, stagger);
+using simd::Vec_f_t;
+
+inline Vec_f_t
+pos_simd(const Grid& g, int i, int n, int stagger) {
+  Vec_f_t result = simd::vec_inc * g.delta[i] + g.pos(i, n, stagger);
   return result;
 }
 
-inline simd::Vec_f_t pos_simd(const Grid& g, int i, int n, bool stagger) {
+inline Vec_f_t
+pos_simd(const Grid& g, int i, int n, bool stagger) {
   return pos_simd(g, i, n, (int)stagger);
 }
 #endif
@@ -27,83 +31,99 @@ inline simd::Vec_f_t pos_simd(const Grid& g, int i, int n, bool stagger) {
 namespace SPH {
 
 #ifdef USE_SIMD
-inline simd::Vec_f_t get_x_simd(const simd::Vec_f_t& r) { return log(r); }
+inline Vec_f_t
+get_x_simd(const Vec_f_t& r) {
+  return log(r);
+}
 
-inline simd::Vec_f_t get_r_simd(const simd::Vec_f_t& x, const simd::Vec_f_t& y,
-                                const simd::Vec_f_t& z) {
+inline Vec_f_t
+get_r_simd(const Vec_f_t& x, const Vec_f_t& y, const Vec_f_t& z) {
   return exp(x);
 }
 
-inline simd::Vec_f_t get_th_simd(const simd::Vec_f_t& x, const simd::Vec_f_t& y,
-                                 const simd::Vec_f_t& z) {
+inline Vec_f_t
+get_th_simd(const Vec_f_t& x, const Vec_f_t& y, const Vec_f_t& z) {
   return y;
 }
 
-inline simd::Vec_f_t get_gamma_d11_simd(const simd::Vec_f_t& x,
-                                        const simd::Vec_f_t& y,
-                                        const simd::Vec_f_t& z) {
+inline Vec_f_t
+get_gamma_d11_simd(const Vec_f_t& x, const Vec_f_t& y,
+                   const Vec_f_t& z) {
   auto r = get_r_simd(x, y, z);
   return r * r;
 }
 
-inline simd::Vec_f_t get_gamma_d22_simd(const simd::Vec_f_t& x,
-                                        const simd::Vec_f_t& y,
-                                        const simd::Vec_f_t& z) {
+inline Vec_f_t
+get_gamma_d22_simd(const Vec_f_t& x, const Vec_f_t& y,
+                   const Vec_f_t& z) {
   auto r = get_r_simd(x, y, z);
   return r * r;
 }
 
-inline simd::Vec_f_t get_gamma_d33_simd(const simd::Vec_f_t& x,
-                                        const simd::Vec_f_t& y,
-                                        const simd::Vec_f_t& z) {
+inline Vec_f_t
+get_gamma_d33_simd(const Vec_f_t& x, const Vec_f_t& y,
+                   const Vec_f_t& z) {
   auto r = get_r_simd(x, y, z);
   auto th = get_th_simd(x, y, z);
   return square(r * sin(th));
 }
 
-inline simd::Vec_f_t get_gamma_simd(const simd::Vec_f_t& x,
-                                    const simd::Vec_f_t& y,
-                                    const simd::Vec_f_t& z) {
+inline Vec_f_t
+get_gamma_simd(const Vec_f_t& x, const Vec_f_t& y, const Vec_f_t& z) {
   return get_gamma_d11_simd(x, y, z) * get_gamma_d22_simd(x, y, z) *
          get_gamma_d33_simd(x, y, z);
 }
 
-inline simd::Vec_f_t get_sqrt_gamma_simd(const simd::Vec_f_t& x,
-                                         const simd::Vec_f_t& y,
-                                         const simd::Vec_f_t& z) {
+inline Vec_f_t
+get_sqrt_gamma_simd(const Vec_f_t& x, const Vec_f_t& y,
+                    const Vec_f_t& z) {
   return sqrt(get_gamma_simd(x, y, z));
 }
 
 #endif
 
-HD_INLINE Scalar get_x(Scalar r) { return log(r); }
+HD_INLINE Scalar
+get_x(Scalar r) {
+  return log(r);
+}
 
-HD_INLINE Scalar get_r(Scalar x, Scalar y, Scalar z) { return exp(x); }
+HD_INLINE Scalar
+get_r(Scalar x, Scalar y, Scalar z) {
+  return exp(x);
+}
 
-HD_INLINE Scalar get_th(Scalar x, Scalar y, Scalar z) { return y; }
+HD_INLINE Scalar
+get_th(Scalar x, Scalar y, Scalar z) {
+  return y;
+}
 
-HD_INLINE Scalar get_gamma_d11(Scalar x, Scalar y, Scalar z) {
+HD_INLINE Scalar
+get_gamma_d11(Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(x, y, z);
   return r * r;
 }
 
-HD_INLINE Scalar get_gamma_d22(Scalar x, Scalar y, Scalar z) {
+HD_INLINE Scalar
+get_gamma_d22(Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(x, y, z);
   return r * r;
 }
 
-HD_INLINE Scalar get_gamma_d33(Scalar x, Scalar y, Scalar z) {
+HD_INLINE Scalar
+get_gamma_d33(Scalar x, Scalar y, Scalar z) {
   Scalar r = get_r(x, y, z);
   Scalar th = get_th(x, y, z);
   return square(r * sin(th));
 }
 
-HD_INLINE Scalar get_gamma(Scalar x, Scalar y, Scalar z) {
+HD_INLINE Scalar
+get_gamma(Scalar x, Scalar y, Scalar z) {
   return get_gamma_d11(x, y, z) * get_gamma_d22(x, y, z) *
          get_gamma_d33(x, y, z);
 }
 
-HD_INLINE Scalar get_sqrt_gamma(Scalar x, Scalar y, Scalar z) {
+HD_INLINE Scalar
+get_sqrt_gamma(Scalar x, Scalar y, Scalar z) {
   return std::sqrt(get_gamma(x, y, z));
 }
 
